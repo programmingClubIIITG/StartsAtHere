5354077a5335600d2a512f5206b366e41dfe578a
realnimish <46227078+realnimish@users.noreply.github.com>
2019-10-01 19:11:58 +0530

    Finds the minimum value in an array

---
+++ minimumElement.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int getMin(int arr[],int size)
+{
+	int ans=arr[0];
+	
+	for(int i=1;i<size;i++)
+	{
+		if(arr[i]<ans) ans = arr[i];
+	}
+	return ans;
+}
+ 
